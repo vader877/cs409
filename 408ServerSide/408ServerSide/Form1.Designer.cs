0a76d6c7aa96bfe006a20f4ea06cc34e97b9efb1
vader877 <tanalpkonuk@sabanciuniv.edu>
2015-10-23 23:43:17 +0300

    kekmaster strikes again

--- 408ServerSide/408ServerSide/Form1.Designer.cs
+++ 408ServerSide/408ServerSide/Form1.Designer.cs
@@ -28,54 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.infoBox = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.listButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // infoBox
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(382, 173);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.infoBox.Location = new System.Drawing.Point(213, 12);
+            this.infoBox.Name = "infoBox";
+            this.infoBox.Size = new System.Drawing.Size(393, 184);
+            this.infoBox.TabIndex = 0;
+            this.infoBox.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 185);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(66, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Server IP";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 185);
+            this.textBox1.Location = new System.Drawing.Point(98, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 2;
             // 
-            // button1
+            // label2
             // 
-            this.button1.Location = new System.Drawing.Point(70, 323);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server Port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Location = new System.Drawing.Point(15, 143);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(75, 23);
+            this.startStopButton.TabIndex = 6;
+            this.startStopButton.Text = "Start";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            // 
+            // listButton
+            // 
+            this.listButton.Location = new System.Drawing.Point(15, 171);
+            this.listButton.Name = "listButton";
+            this.listButton.Size = new System.Drawing.Size(116, 23);
+            this.listButton.TabIndex = 7;
+            this.listButton.Text = "Get Player List";
+            this.listButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 488);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(618, 206);
+            this.Controls.Add(this.listButton);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.infoBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -85,10 +116,13 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox infoBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Button listButton;
     }
 }
 
