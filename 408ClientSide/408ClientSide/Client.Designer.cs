56d2522eebac3ceb7d6a512ae336bab04fadce14
dnuhoglu <dangerdox@hotmail.com>
2015-10-24 01:15:18 +0300

    click+list+exit

--- 408ClientSide/408ClientSide/Client.Designer.cs
+++ 408ClientSide/408ClientSide/Client.Designer.cs
@@ -33,7 +33,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.ipServer = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.displayScreen = new System.Windows.Forms.RichTextBox();
             this.playerName = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.portNumber = new System.Windows.Forms.NumericUpDown();
@@ -45,115 +45,104 @@
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(87, 455);
-            this.connectButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.connectButton.Location = new System.Drawing.Point(65, 370);
             this.connectButton.Name = "connectButton";
-            this.connectButton.Size = new System.Drawing.Size(105, 57);
+            this.connectButton.Size = new System.Drawing.Size(79, 46);
             this.connectButton.TabIndex = 0;
             this.connectButton.Text = "Connect";
             this.connectButton.UseVisualStyleBackColor = true;
             // 
             // listButton
             // 
-            this.listButton.Location = new System.Drawing.Point(236, 455);
-            this.listButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listButton.Location = new System.Drawing.Point(177, 370);
             this.listButton.Name = "listButton";
-            this.listButton.Size = new System.Drawing.Size(105, 57);
+            this.listButton.Size = new System.Drawing.Size(79, 46);
             this.listButton.TabIndex = 1;
             this.listButton.Text = "Player List";
             this.listButton.UseVisualStyleBackColor = true;
+            this.listButton.Click += new System.EventHandler(this.listButton_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 80);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(5, 65);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Server IP:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(40, 111);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(30, 90);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Port:";
             // 
             // ipServer
             // 
-            this.ipServer.Location = new System.Drawing.Point(87, 76);
-            this.ipServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ipServer.Location = new System.Drawing.Point(65, 62);
             this.ipServer.Name = "ipServer";
-            this.ipServer.Size = new System.Drawing.Size(132, 22);
+            this.ipServer.Size = new System.Drawing.Size(100, 20);
             this.ipServer.TabIndex = 10;
             // 
-            // richTextBox1
+            // displayScreen
             // 
-            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.richTextBox1.Location = new System.Drawing.Point(87, 162);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(395, 267);
-            this.richTextBox1.TabIndex = 12;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.displayScreen.BackColor = System.Drawing.SystemColors.Window;
+            this.displayScreen.Location = new System.Drawing.Point(65, 132);
+            this.displayScreen.Name = "displayScreen";
+            this.displayScreen.ReadOnly = true;
+            this.displayScreen.Size = new System.Drawing.Size(297, 218);
+            this.displayScreen.TabIndex = 12;
+            this.displayScreen.Text = "";
+            this.displayScreen.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // playerName
             // 
-            this.playerName.Location = new System.Drawing.Point(87, 15);
-            this.playerName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playerName.Location = new System.Drawing.Point(65, 12);
             this.playerName.Name = "playerName";
-            this.playerName.Size = new System.Drawing.Size(132, 22);
+            this.playerName.Size = new System.Drawing.Size(100, 20);
             this.playerName.TabIndex = 13;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 18);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(21, 15);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 17);
+            this.label5.Size = new System.Drawing.Size(38, 13);
             this.label5.TabIndex = 14;
             this.label5.Text = "Name:";
             // 
             // portNumber
             // 
-            this.portNumber.Location = new System.Drawing.Point(87, 108);
-            this.portNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.portNumber.Location = new System.Drawing.Point(65, 88);
             this.portNumber.Name = "portNumber";
-            this.portNumber.Size = new System.Drawing.Size(133, 22);
+            this.portNumber.Size = new System.Drawing.Size(100, 20);
             this.portNumber.TabIndex = 16;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(288, 80);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(216, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 17;
             this.label1.Text = "Status:";
             // 
             // statusText
             // 
             this.statusText.BackColor = System.Drawing.SystemColors.Menu;
-            this.statusText.Location = new System.Drawing.Point(349, 80);
-            this.statusText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.statusText.Location = new System.Drawing.Point(262, 65);
             this.statusText.Name = "statusText";
-            this.statusText.Size = new System.Drawing.Size(132, 22);
+            this.statusText.Size = new System.Drawing.Size(100, 20);
             this.statusText.TabIndex = 18;
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(377, 455);
-            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Location = new System.Drawing.Point(283, 370);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(105, 57);
+            this.exitButton.Size = new System.Drawing.Size(79, 46);
             this.exitButton.TabIndex = 19;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
@@ -161,22 +150,21 @@
             // 
             // Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 549);
+            this.ClientSize = new System.Drawing.Size(432, 446);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.statusText);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.portNumber);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.playerName);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.displayScreen);
             this.Controls.Add(this.ipServer);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.listButton);
             this.Controls.Add(this.connectButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Client";
             this.Text = "Client";
             ((System.ComponentModel.ISupportInitialize)(this.portNumber)).EndInit();
@@ -192,7 +180,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox ipServer;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox displayScreen;
         private System.Windows.Forms.TextBox playerName;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.NumericUpDown portNumber;
--- 408ClientSide/408ClientSide/Client.cs
+++ 408ClientSide/408ClientSide/Client.cs
@@ -22,6 +22,8 @@ namespace _408ClientSide
             {
                 try
                 {
+                    // establishing connection to the server 
+
                     connectButton.Text = "Disconnect";
                     byte ipPart1 = Convert.ToByte(ipServer.Text.Substring(0, 3));
                     byte ipPart2 = Convert.ToByte(ipServer.Text.Substring(4, 3));
@@ -30,14 +32,45 @@ namespace _408ClientSide
                     string ipFinal = ipPart1.ToString() + "." + ipPart2.ToString() + "." + ipPart3.ToString() + "." + ipPart4.ToString();
                     int portNo = (int)portNumber.Value;
                     newClient.Connect(ipFinal, portNo);
-                    statusText.Text = "CONNECTED";
-                    isConnected = true;
+                    displayScreen.AppendText("Establising connection to the server...\n");
+
+                    // sending playerName to the server
+
+                    NetworkStream serverStream = newClient.GetStream();
+                    byte[] sentStream = System.Text.Encoding.ASCII.GetBytes(playerName.Text + "$");
+                    serverStream.Write(sentStream, 0, sentStream.Length);
+                    serverStream.Flush();
+
+                    displayScreen.AppendText("Checking for " + playerName + "...\n");
+
+                    // receiving acknowledgement from the server about if playerName is unique or not
+
+                    byte[] approvalByte = new byte[1024];
+                    serverStream.Read(approvalByte, 0, (int)approvalByte.Length);
+                    string approvalString = System.Text.Encoding.ASCII.GetString(approvalByte);
+
+                    if (approvalString == "y")
+                    {
+
+                        isConnected = true;
+                        displayScreen.AppendText("Connection successful!\n\n");
+                        statusText.Text = "CONNECTED";
+                    }
+
+                    else
+                    {
+                        displayScreen.AppendText("Player already exists. Please try a different name.\n\n");
+                        connectButton.Text = "Connect";
+                        newClient.Close();
+                        playerName.Clear();
+                    }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message, "An error is occurred while trying to connect to the server.", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+
             else
             {
                 newClient.Close();
@@ -55,6 +88,28 @@ namespace _408ClientSide
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void listButton_Click(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = newClient.GetStream();
+            byte[] sentStream = System.Text.Encoding.ASCII.GetBytes("PL$");
+            serverStream.Write(sentStream, 0, sentStream.Length);
+            serverStream.Flush();
+
+            displayScreen.AppendText("Generating the list of online players...\n\n");
+
+            byte[] PLByte = new byte[1024];
+            serverStream.Read(PLByte, 0, (int)PLByte.Length);
+            string PLString = System.Text.Encoding.ASCII.GetString(PLByte);
+            string[] words = PLString.Split('$');
+            
+            for(int i=0; i<words.Length; i++)
+            {
+                displayScreen.AppendText(words[0] + "\n");
+            }
+
+            displayScreen.AppendText("\n");
         }
     }
 }
