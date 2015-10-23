9c8cb126eed784d60cefb54d49de1fab2274018c
dnuhoglu <dangerdox@hotmail.com>
2015-10-23 22:03:30 +0300

    22:03

--- 408ClientSide/408ClientSide/Form1.Designer.cs
+++ 408ClientSide/408ClientSide/Form1.Designer.cs
@@ -30,22 +30,24 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.ipBox = new System.Windows.Forms.TextBox();
-            this.portBox = new System.Windows.Forms.TextBox();
+            this.ipBoxL = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ipBoxS = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.playerName = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.portBoxL = new System.Windows.Forms.NumericUpDown();
+            this.portBoxS = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.portBoxL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portBoxS)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 356);
+            this.button1.Location = new System.Drawing.Point(65, 370);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(79, 46);
             this.button1.TabIndex = 0;
@@ -54,26 +56,19 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(323, 356);
+            this.button2.Location = new System.Drawing.Point(323, 370);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(79, 46);
             this.button2.TabIndex = 1;
             this.button2.Text = "Player List";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // ipBox
+            // ipBoxL
             // 
-            this.ipBox.Location = new System.Drawing.Point(65, 67);
-            this.ipBox.Name = "ipBox";
-            this.ipBox.Size = new System.Drawing.Size(130, 20);
-            this.ipBox.TabIndex = 4;
-            // 
-            // portBox
-            // 
-            this.portBox.Location = new System.Drawing.Point(65, 93);
-            this.portBox.Name = "portBox";
-            this.portBox.Size = new System.Drawing.Size(130, 20);
-            this.portBox.TabIndex = 5;
+            this.ipBoxL.Location = new System.Drawing.Point(65, 67);
+            this.ipBoxL.Name = "ipBoxL";
+            this.ipBoxL.Size = new System.Drawing.Size(130, 20);
+            this.ipBoxL.TabIndex = 4;
             // 
             // label1
             // 
@@ -88,7 +83,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 96);
+            this.label2.Location = new System.Drawing.Point(30, 95);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 7;
@@ -106,30 +101,25 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(237, 96);
+            this.label4.Location = new System.Drawing.Point(237, 95);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(29, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Port:";
             // 
-            // textBox1
+            // ipBoxS
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 67);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 20);
-            this.textBox1.TabIndex = 10;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(272, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 20);
-            this.textBox2.TabIndex = 11;
+            this.ipBoxS.Location = new System.Drawing.Point(272, 67);
+            this.ipBoxS.Name = "ipBoxS";
+            this.ipBoxS.Size = new System.Drawing.Size(130, 20);
+            this.ipBoxS.TabIndex = 10;
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
             this.richTextBox1.Location = new System.Drawing.Point(65, 132);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(337, 218);
             this.richTextBox1.TabIndex = 12;
             this.richTextBox1.Text = "";
@@ -150,26 +140,42 @@
             this.label5.TabIndex = 14;
             this.label5.Text = "Name:";
             // 
+            // portBoxL
+            // 
+            this.portBoxL.Location = new System.Drawing.Point(65, 93);
+            this.portBoxL.Name = "portBoxL";
+            this.portBoxL.Size = new System.Drawing.Size(130, 20);
+            this.portBoxL.TabIndex = 15;
+            // 
+            // portBoxS
+            // 
+            this.portBoxS.Location = new System.Drawing.Point(272, 93);
+            this.portBoxS.Name = "portBoxS";
+            this.portBoxS.Size = new System.Drawing.Size(130, 20);
+            this.portBoxS.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(447, 446);
+            this.Controls.Add(this.portBoxS);
+            this.Controls.Add(this.portBoxL);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.playerName);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ipBoxS);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.portBox);
-            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.ipBoxL);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.portBoxL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portBoxS)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,17 +185,17 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox ipBox;
-        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.TextBox ipBoxL;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox ipBoxS;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox playerName;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown portBoxL;
+        private System.Windows.Forms.NumericUpDown portBoxS;
     }
 }
 
